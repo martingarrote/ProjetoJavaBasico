3ab2db9f271961d612954cabe2426b6620eaad56
martingarrote <martindambrowskigarrote@gmail.com>
2023-05-12 18:17:49 -0300

    Implementar a função getMaxGradeStudents

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,7 @@ public class Main {
 
         getApprovedStudents(allStudents);
         getReprovedStudents(allStudents);
-
-        // Agora vamos as atividades
-        /*
-
-        1. Recupere da lista os alunos que passaram de ano (nota minima 7.0).
-            - Exiba os dados nesse formato: <código> - <nome> : Média = <nota>
-        2. Recupere da lista os alunos que não passaram de ano.
-            - Exiba os dados nesse formato: <código> - <nome> : Média = <media> (Faltou = <nota_faltante>)
-        3. Traga os alunos que tiraram a nota máxima (nota 10).
-            - Exiba os dados nesse formato: <código> - <nome>
-        4. Traga o aluno que tirou a menor nota, em caso de notas iguais, traga ambos os alunos.
-            - Exiba os dados nesse formato: <código> - <nome> : Nota = <nota>
-        5. Faça uma lista com top 3 notas de alunos. Em caso de notas iguais coloque todos na mesma posição.
-            - Ex:
-                1º - Fulano : Nota = 10.0;
-                   - Beltrano : Nota = 10.0;
-                2º - Joãozinho : Nota = 9.0;
-                3º - Mariazinha : Nota = 8.9;
-            - Exiba os dados nesse formato: <posicao> - <nome> : Nota = <nota>
-        6. Faça uma lista com as 3 menores notas de alunos. Em caso de notas iguais coloque todos na mesma posição. Exemplo igual a anterior
-            - Exiba os dados nesse formato: <posicao> - <nome> : Nota = <nota>
-        7. Monte a média de todos os alunos e exiba em tela ordenando da maior para a menor nota.
-            - Exiba os dados nesse formato: <posicao> - <código> - <nome> : Média = <nota>
-
-         */
+        getMaxGradeStudents(allStudents);
     }
 
     static void getApprovedStudents(List<Student> studentsList) {
@@ -53,6 +29,20 @@ public class Main {
             if (average < 7) {
                 System.out.printf("%d - %s : Média = %f (Faltou = %f)\n", s.getCode(), s.getName(), average, missing);
             }
+        }
+    }
+
+    static void getMaxGradeStudents(List<Student> studentList) {
+        var count = 0;
+        for (Student s : studentList) {
+            var average = (s.getTestOne() + s.getTestTwo() + s.getTestThree())/3;
+            if (average == 10) {
+                count++;
+                System.out.printf("%d - %s\n", s.getCode(), s.getName());
+            }
+        }
+        if (count == 0) {
+            System.out.println("Nenhum estudante alcançou a nota máxima.");
         }
     }
 }
