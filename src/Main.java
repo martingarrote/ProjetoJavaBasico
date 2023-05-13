c5143f3cc70fd2973a8b7cf91551565b53e6af67
martingarrote <martindambrowskigarrote@gmail.com>
2023-05-13 19:09:00 -0300

    Implementar a função getLowestGradeStudents
    e também a função getLowestGrade

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import builders.StudentsBuilder;
 import entities.Student;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -12,11 +15,12 @@ public class Main {
         getApprovedStudents(allStudents);
         getReprovedStudents(allStudents);
         getMaxGradeStudents(allStudents);
+        getLowestGradeStudents(allStudents);
     }
 
     static void getApprovedStudents(List<Student> studentsList) {
         for (Student s : studentsList) {
-            var average = (s.getTestOne() + s.getTestTwo() + s.getTestThree())/3;
+            var average = (s.getTestOne() + s.getTestTwo() + s.getTestThree()) / 3;
             if (average >= 7) {
                 System.out.printf("%d - %s : Média = %.2f\n", s.getCode(), s.getName(), average);
             }
@@ -25,7 +29,7 @@ public class Main {
 
     static void getReprovedStudents(List<Student> studentsList) {
         for (Student s : studentsList) {
-            var average = (s.getTestOne() + s.getTestTwo() + s.getTestThree())/3;
+            var average = (s.getTestOne() + s.getTestTwo() + s.getTestThree()) / 3;
             var missing = 7 - average;
             if (average < 7) {
                 System.out.printf("%d - %s : Média = %.2f (Faltou = %.2f)\n", s.getCode(), s.getName(), average, missing);
@@ -39,12 +43,41 @@ public class Main {
         };
         var studentsWithMaxGrade = studentsList.stream().filter(maxGrade).toList();
         if (studentsWithMaxGrade.size() > 0) {
-            System.out.println("Estudantes que obtiveram nota máxima:");
             for (Student s : studentsWithMaxGrade) {
                 System.out.printf("%d - %s\n", s.getCode(), s.getName());
             }
         } else {
             System.out.println("Nenhum estudante obteve nota máxima");
+        }
+    }
+
+    static float getLowestGrade(Student s) {
+        var lowestGrade = s.getTestOne();
+        if (s.getTestTwo() < lowestGrade) {
+            lowestGrade = s.getTestTwo();
+        }
+        if (s.getTestThree() < lowestGrade) {
+            lowestGrade = s.getTestThree();
+        }
+        return lowestGrade;
+    }
+
+    static void getLowestGradeStudents(List<Student> studentsList) {
+
+        var lowestGradeStudents = new ArrayList<Student>();
+        var lowestGrade = getLowestGrade(studentsList.get(0));
+
+        for (Student s : studentsList) {
+            var studentGrade = getLowestGrade(s);
+            if (studentGrade < lowestGrade) {
+                lowestGradeStudents.clear();
+                lowestGradeStudents.add(s);
+            } else if (studentGrade == lowestGrade) {
+                lowestGradeStudents.add(s);
+            }
+        }
+        for (Student s : lowestGradeStudents) {
+            System.out.printf("%d - %s : Nota: %.2f\n", s.getCode(), s.getName(), getLowestGrade(s));
         }
     }
 }
